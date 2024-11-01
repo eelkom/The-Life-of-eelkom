62963dabcc71fc6509f792eff1ea0da41663a562
CHANGMOK LEE <cmlee5075@gmail.com>
2024-11-02 01:43:58 +0900

    [Refactor]soundBoard

--- js/webAudio.js
+++ js/webAudio.js
@@ -2,7 +2,33 @@
     const keys = ['upE', 'E', 'upDs', 'Ds', 'upCs', 'Cs', 'A', 'Gs'];
     const keyMap = ['q', 'w', 'a', 's', 'd', 'z', 'x', 'c'];
     const audioElements = {};
-    const START_TIME = 0.13;
+    // const START_TIME = 0.13;
+    const START_TIME = 0;
+
+    const sounds = [
+        'Work_it',
+        'Make_it',
+        'Do_it',
+        'Makes_us',
+        'Harder',
+        'Better',
+        'Faster',
+        'Stronger',
+        'More_than',
+        'Hour',
+        'Our',
+        'Never',
+        'Ever',
+        'After',
+        'Work_is',
+        'Over',
+        'All',
+        'Drum',
+        '',
+    ];
+    const soundMap = ['q', 'w', 'e', 'r', 'a', 's', 'd', 'f', 't', 'y', 'u', 'i', 'g', 'h', 'j', 'k', 'z', 'x', 'undefined'];
+    const audioElements2 = {};
+    const START_TIME2 = 0;
 
     function init() {
         const c = document.querySelector(".soundboard");
@@ -50,5 +76,45 @@
         audio.play();
     }
 
+    function init2() {
+        const c = document.querySelector(".soundboard2");
+
+        for (let i = 0; i < sounds.length; i++) {
+            // content
+            let content = document.createElement('div');
+            content.classList.add('content');
+            content.textContent = `${sounds[i]} (${soundMap[i]})`;
+            c.appendChild(content);
+
+            //Create audioElem
+            let audioElem = new Audio(`audio_daft/${sounds[i]}.wav`);
+            audioElem.type = 'audio/wav';
+            audioElements[soundMap[i]] = audioElem;
+
+            // content style
+            content.style.perspective = '600px';
+            content.style.transformStyle = 'preserve-3d';
+            content.style.transition = 'transform 0.7s ease';
+
+            // content EventListner
+            content.addEventListener('click', () => {
+                playAudio(audioElem);
+            });
+            content.addEventListener('mouseover', () => {
+                content.style.transform = 'rotateY(30deg)';
+            });
+            content.addEventListener('mouseout', () => {
+                content.style.transform = 'rotateY(0deg)';
+            });
+        }
+
+        window.addEventListener('keydown', (e) => {
+            if (audioElements2[e.key]) {
+                playAudio(audioElements2[e.key]);
+            }
+        });
+    }
+
     init();
+    init2();
 })();
